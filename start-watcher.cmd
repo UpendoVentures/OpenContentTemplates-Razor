81fdd4b83e46e4e4a2a397cbc9c676d87a2fe3ca
Will Strohl <me@willstrohl.com>
2026-04-23 17:00:47 -0700

    Added watcher features for faster development

---
+++ start-watcher.cmd
@@ -0,0 +1,2 @@
+@echo off
+start "Template Watcher" powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Work\Dnn.Modules\OpenContentTemplates-Razor\OpenContentTemplates-Razor\Watch-And-Copy.ps1"
